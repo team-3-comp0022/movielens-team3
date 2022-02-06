975c82d7c92675934c4f478940f72708e1867a35
alexhein189 <zcabhei@ucl.ac.uk>
2022-02-06 16:49:15 +0000

    setting up database through js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,3 +1,5 @@
+const queries = require('./queries.js')
+
 var mysql = require('mysql')
 var express = require('express')
 
@@ -5,7 +7,7 @@ var connection = mysql.createConnection({
     host: "127.0.0.1",
     user: "root",
     password: "example",
-    database: "sys",
+    database: "films",
     port: '8086'
   })
 
@@ -17,19 +19,70 @@ connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
   console.log('The solution is: ', rows[0].solution)
 })
 
-const query = `
-CREATE TABLE ratings (
-    userId int,
-    movieId int,
-    rating int,
-    timestamp int
-)
-`;
+/* create tables */
+// const create_list = queries.create_list
 
-connection.query(query, function (err, rows, fields) {
+// create_list.forEach(function (query) {
+//     connection.query(query, function (err, rows, fields) {
+//         if (err) throw err
+      
+//         console.log('Success creating tables')
+//       });
+// });
+
+/* delete tables */
+const drop_all = queries.drop_all
+
+connection.query(drop_all, function (err, rows, fields) {
     if (err) throw err
   
     console.log('Success')
   })
+
+
+/* import csv */
+// const fs = require("fs");
+// const fastcsv = require("fast-csv");
+
+// const filenames = queries.filenames
+// const csv_queres = queries.csv_queres
+
+// filenames.forEach((file, index)=>{
+//     const csv_query = csv_queres[index];
+//     let stream = fs.createReadStream(file);
+//     let csvData = [];
+//     let csvStream = fastcsv
+//     .parse()
+//     .on("data", function(data) {
+//         csvData.push(data);
+//     })
+//     .on("end", function() {
+//         // remove the first line: header
+//         csvData.shift();
+        
+//         // open the connection
+//         // let query =
+//         //     csv_query;
+//         //     connection.query(query, [csvData], (error, response) => {
+//         //     console.log(error || response);
+//         //     connection.end();
+//         //     });
+//         connection.connect(error => {
+//             if (error) {
+//               console.error(error);
+//             } else {
+//               let query =
+//                 "INSERT INTO category (id, name, description, created_at) VALUES ?";
+//               connection.query(query, [csvData], (error, response) => {
+//                 console.log(error || response);
+//               });
+//             }
+//         });    
+//     });
+//     stream.pipe(csvStream);
+// });
+
   
-connection.end()
+connection.end();
+
+
