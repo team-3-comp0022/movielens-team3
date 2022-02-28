14431cec533b562083158f88843a81a5200665d9
Alexandra Irimia <alexandrairimia27@yahoo.com>
2022-02-28 18:00:21 +0000

    Update CSS parts for MovieGrid component

--- frontend/src/components/movie-grid/MovieGrid.jsx
+++ frontend/src/components/movie-grid/MovieGrid.jsx
@@ -280,6 +280,30 @@ return (
             </div>
              
              </div>
+
+
+{/* 
+                <div id="form-wrapper">
+                <form action="/p/quote.php" method="GET">
+                    <h1 id="form-title">Select Debt Amount</h1>
+                    <div id="debt-amount-slider">
+                    <input type="radio" name="debt-amount" id="1" value="1" required></input>
+                    <label for="1" data-debt-amount="< $10k"></label>
+                    <input type="radio" name="debt-amount" id="2" value="2" required></input>
+                    <label for="2" data-debt-amount="$10k-25k"></label>
+                    <input type="radio" name="debt-amount" id="3" value="3" required></input>
+                    <label for="3" data-debt-amount="$25k-50k"></label>
+                    <input type="radio" name="debt-amount" id="4" value="4" required></input>
+                    <label for="4" data-debt-amount="$50k-100k"></label>
+                    <input type="radio" name="debt-amount" id="5" value="5" required></input>
+                    <label for="5" data-debt-amount="$100k+"></label>
+                    <div id="debt-amount-pos"></div>
+                    </div>
+                </form>
+                <button type="submit">Get Debt Free!</button>
+                </div> */}
+
+
            
 	   </div>
 
