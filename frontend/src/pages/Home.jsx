4d24ea30af6bdf585cdc1cc5b44d44f575b8560c
Alexandra Irimia <alexandrairimia27@yahoo.com>
2022-03-13 09:46:57 +0000

    update the home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -63,12 +63,22 @@ const Home = () => {
 
                 <div className="section mb-3">
                     <div className="section__header mb-2">
-                        <h2>Most Popular Movies</h2>
+                        <h2>Top Rated Movies</h2>
                         <Link to="/movie">
                             <OutlineButton className="small">View more</OutlineButton>
                         </Link>
                     </div>
                     <MovieList category={category.popular} type={movieType.top_rated}/>
+                    
+                </div>
+
+                <div className="section mb-3">
+                    <div className="section__header mb-2">
+                        <h2>Most Popular Movies</h2>
+                        <Link to="/movie">
+                            <OutlineButton className="small">View more</OutlineButton>
+                        </Link>
+                    </div>
                     
                 </div>
 
