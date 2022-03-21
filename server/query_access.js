0d4e0ffaa702f0a9737ceef6211e41c321f667e8
alexhein189 <zcabhei@ucl.ac.uk>
2022-03-21 12:19:46 +0000

    new foreign key

--- server/query_access.js
+++ server/query_access.js
@@ -39,9 +39,9 @@ var connection = mysql.createConnection({
 // `
 
 function initialise_data(){
-    deleteDatabase()
-    makeDatabase()
-    useDatabase()
+    //deleteDatabase()
+    // makeDatabase()
+    // useDatabase()
     makeTables()
     addData()
     const sleep = (waitTimeInMs) => new Promise(resolve => setTimeout(resolve, waitTimeInMs));
@@ -65,7 +65,7 @@ getReportData(862, function(result){
 });
 */
 
-// initialise_data()
+initialise_data()
 // deleteTables()
 
 //TESTNG FIFTH QUEREY AND SIXTH QUERY
